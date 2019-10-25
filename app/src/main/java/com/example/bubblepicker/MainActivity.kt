f40e525f6b62b5b9ff534536f0273029e0608b7b
Kkangjii <l6mTbdko9Nza@gmail.com>
2019-10-25 11:31:56 +0700

    import finger print

--- app/src/main/java/com/example/bubblepicker/MainActivity.kt
+++ app/src/main/java/com/example/bubblepicker/MainActivity.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.hardware.fingerprint.FingerprintManagerCompat.CryptoObject
+import com.easyfingerprint.EasyFingerPrint
 import com.example.bubblepicker.View.Menu1Fragment
 import com.example.bubblepicker.View.Menu2Fragment
 import com.example.bubblepicker.View.Menu3Fragment
 import com.example.bubblepicker.View.Menu4Fragment
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.FirebaseAuth
 import com.luseen.luseenbottomnavigation.BottomNavigation.BottomNavigationItem
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -53,6 +53,36 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    fun biometic(){
+        EasyFingerPrint(this)
+            .setTittle("Sign in")
+            .setSubTittle("Login With Fingers Print")
+            .setDescription("In order to use the Fingerprint sensor we need your authorization first.e")
+            .setColorPrimary(R.color.colorPrimary)
+            .setIcon(ContextCompat.getDrawable(this,R.mipmap.ic_launcher_round))
+            .setListern(object : EasyFingerPrint.ResultFingerPrintListern{
+                override fun onError(mensage: String, code: Int) {
+
+                    when(code){
+                        EasyFingerPrint.CODE_ERRO_CANCEL -> { } // TO DO
+                        EasyFingerPrint.CODE_ERRO_GREATER_ANDROID_M -> { } // TO DO
+                        EasyFingerPrint.CODE_ERRO_HARDWARE_NOT_SUPPORTED -> { } // TO DO
+                        EasyFingerPrint.CODE_ERRO_NOT_ABLED -> { } // TO DO
+                        EasyFingerPrint.CODE_ERRO_NOT_FINGERS -> { } // TO DO
+                        EasyFingerPrint.CODE_NOT_PERMISSION_BIOMETRIC -> { } // TO DO
+                    }
+
+                    Toast.makeText(this@MainActivity,"Error: $mensage / $code",Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onSucess(cryptoObject: CryptoObject?) {
+                    Toast.makeText(this@MainActivity,"Sucess",Toast.LENGTH_SHORT).show()
+                }
+
+            })
+            .startScan()
     }
 
 
--- app/src/main/java/com/example/bubblepicker/View/Menu3Fragment.kt
+++ app/src/main/java/com/example/bubblepicker/View/Menu3Fragment.kt
@@ -6,39 +6,61 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.bubblepicker.MainActivity
 import com.example.bubblepicker.R
+import com.github.pwittchen.rxbiometric.library.validation.Preconditions
 import com.google.firebase.auth.FirebaseAuth
+import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.fragmentmenu3.*
 
 class Menu3Fragment: Fragment() {
 
     private lateinit var auth: FirebaseAuth
-
+    private var disposable: Disposable? = null
+    private lateinit var mainActivity:MainActivity
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         auth = FirebaseAuth.getInstance()
+        mainActivity  = activity as MainActivity
 
         regis.setOnClickListener {
-            auth.createUserWithEmailAndPassword(username.text.toString(), pass.text.toString()).addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(context, "Successfully Registered", Toast.LENGTH_LONG).show()
+            if (username.text.toString().isEmpty()||pass.text.toString().isEmpty()){
+                Toast.makeText(context,"Plase in put Username Or Password ",Toast.LENGTH_SHORT).show()
+            }
+            else {
+                auth.createUserWithEmailAndPassword(username.text.toString(), pass.text.toString())
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(context, "Successfully Registered", Toast.LENGTH_LONG).show()
 
-                } else {
-                    Toast.makeText(context, "Registration Failed", Toast.LENGTH_LONG).show()
-                }
+                        } else {
+                            Toast.makeText(context, "Registration Failed", Toast.LENGTH_LONG).show()
+                        }
+                    }
             }
         }
         signin.setOnClickListener {
-            auth.signInWithEmailAndPassword(username.text.toString(), pass.text.toString()).addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(context, "Successfully Login", Toast.LENGTH_LONG).show()
-
-                } else {
-                    Toast.makeText(context, "Registration Failed", Toast.LENGTH_LONG).show()
-                }
+            if (username.text.toString().isEmpty()||pass.text.toString().isEmpty()){
+                Toast.makeText(context,"Plase in put Username Or Password ",Toast.LENGTH_SHORT).show()
             }
+            else {
+                auth.signInWithEmailAndPassword(username.text.toString(), pass.text.toString())
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(context, "Successfully Login", Toast.LENGTH_LONG).show()
+
+                        } else {
+                            Toast.makeText(context, "Registration Failed", Toast.LENGTH_LONG).show()
+                        }
+                    }
+            }
+        }
+        Preconditions.hasBiometricSupport(context!!)
+        button.setOnClickListener {
+            mainActivity!!.biometic()
+
         }
 
 
